645c4562db5251d4ec6ad6bde38fb3c68244e7cb
Steven Roman <sroman@sdcounty.ca.gov>
2025-01-26 16:09:16 -0800

    tree control works now too

--- Consumer/Views/TreeContainer.xaml.cs
+++ Consumer/Views/TreeContainer.xaml.cs
@@ -3,32 +3,27 @@ using CSE.Maui.CustomControls.Consumer.Services;
 
 namespace CSE.Maui.CustomControls.Consumer.Views;
 
-public partial class TreeContainer : ContentView
+public partial class TreeContainer : ContentPage
 {
 
     public required DataService service;
     public required CompanyTreeViewBuilder companyTreeViewBuilder;
 
-    public TreeContainer()
-    {
-        InitializeComponent();
-    }
-
     public TreeContainer(DataService service, CompanyTreeViewBuilder companyTreeViewBuilder)
     {
-        //InitializeComponent();
-        //this.service = service;
-        //this.companyTreeViewBuilder = companyTreeViewBuilder;
+        InitializeComponent();
+        this.service = service;
+        this.companyTreeViewBuilder = companyTreeViewBuilder;
 
-        //ProcessTreeView();
+        ProcessTreeView();
     }
 
-    //private void ProcessTreeView()
-    //{
-    //    var xamlItemGroups = companyTreeViewBuilder.GroupData(service);
-    //    var rootNodes = TheTreeView.ProcessXamlItemGroups(xamlItemGroups);
-    //    TheTreeView.RootNodes = rootNodes;
-    //}
+    private void ProcessTreeView()
+    {
+        var xamlItemGroups = companyTreeViewBuilder.GroupData(service);
+        var rootNodes = TheTreeView.ProcessXamlItemGroups(xamlItemGroups);
+        TheTreeView.RootNodes = rootNodes;
+    }
 
 
 }
